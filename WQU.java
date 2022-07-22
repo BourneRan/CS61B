4d68e89cd062e9fe87c253b317c83c53fd4d318f
Bourne <BourneRan991001@gmail.com>
2022-07-22 12:53:34 +0800

    Add a test.

---
+++ WQU.java
@@ -0,0 +1,13 @@
+/**
+ * Create by:
+ * ${USER}
+ * ${TIME} ${DATE}
+ */
+
+public class WQU {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
